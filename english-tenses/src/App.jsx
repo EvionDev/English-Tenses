15e802313a4419af0f9f9b89a233cea636facbef
Eryk Latasiewicz <eryk.ne05@gmail.com>
2022-12-13 17:54:53 +0100

    update

---
+++ english-tenses/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="h-screen w-screen bg-black"></div>
+    </div>
+  );
+}
+
+export default App;
